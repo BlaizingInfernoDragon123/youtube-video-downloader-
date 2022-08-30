d6c28351f1963392b5af16b9dd1d7717dbe888a2
BlazingInfernoDragon123 <96913755+BlaizingInfernoDragon123@users.noreply.github.com>
2022-08-30 16:25:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,11 @@ function deleteCookie(name) {
     'max-age': -1
   })
 }
+// returns the cookie with the given name,
+// or undefined if not found
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
